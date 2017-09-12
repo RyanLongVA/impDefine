25af10bf06d34391074dc08109a0978fbcf4ab06
Ryan Long <saferkeymercy@gmail.com>
2017-09-12 18:49:24 -0400

    removing unneccesary

--- test.js
+++
@@ -1,8 +0,0 @@
-var page = require('webpage').create();
-page.onResourceRequested = function(request) {
-    console.log('Request ' + JSON.stringify(request.undefined, 4));
-};
-page.onResourceReceived = function(response) {
-    console.log('Recieve ' + JSON.stringify(response, undefined, 4));
-};
-page.open(url);

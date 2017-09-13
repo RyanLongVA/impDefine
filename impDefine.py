f08dddb8d78567baad54f4e68b8732d92104bfd9
Ryan Long <saferkeymercy@gmail.com>
2017-09-13 00:41:45 -0400

    completed hardcoded test

--- impDefine.py
+++ impDefine.py
@@ -1,13 +1,30 @@
-import socket, sys, os, httplib
+#!/usr/bin/env python
+import socket, sys, os, requests, pdb
 
 def main():
     list = filter(None, [list.replace('\n', '') for list in open("start.links", "r").readlines()])
-    print list
-def sslConnect(domain, directory):
-    connection = httplib.HTTPSConnection(domain)
-    connection.request("GET", directory)
-    response = connection.getresponse()
-    return str(response.status) + '\n' + response.reason + '\n' + response.read()
+    cleanList = []
+    for a in list:
+        try:
+            response = sslConnect(a)
+        except requests.ConnectionError:
+            print '[-] %s, timeout error'%(a)
+            continue
+        #print response
+        if "Path not supported" not in response.content:
+            print '[+] %s, clean'%(a)
+            cleanList.append(a)
+        else:
+            print '[-] %s, dirty'%(a)
+        #sslConnect(domain, directory)
+        #if response.status blah blah
+
+#def prefixRemoval(link):
+#    if link.startswith("https://"): return link[8:]
+#    return link[7:] if link.startswith("http://") else link
+
+def sslConnect(domain):
+    connection = requests.get(domain, timeout=3); return connection
 
 #test = sslConnect(sys.argv[1], sys.argv[2])
 #print test

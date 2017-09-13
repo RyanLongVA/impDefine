e1d02ef94a8aadca3252577cc38323d21fa09275
Ryan Long <saferkeymercy@gmail.com>
2017-09-13 14:49:23 -0400

    revamp and multiprocessing

--- impDefine.py
+++ impDefine.py
@@ -1,31 +1,28 @@
 #!/usr/bin/env python
 import socket, sys, os, requests, pdb
-
+import multiprocessing as mp
 def main():
-    list = filter(None, [list.replace('\n', '') for list in open("start.links", "r").readlines()])
+    clist = filter(None, [clist.replace('\n', '') for clist in open("start.links", "r").readlines()])
     cleanList = []
-    for a in list:
-        try:
-            response = sslConnect(a)
-        except requests.ConnectionError:
-            print '[-] %s, timeout error'%(a)
-            continue
-        #print response
+    pool = mp.Pool(4)
+    cleanList += filter(None, pool.map(startList, clist))
+    pool.close()
+
+def startList(a):
+    try:
+        response = sslConnect(a)
         if "Path not supported" not in response.content:
             print '[+] %s, clean'%(a)
-            cleanList.append(a)
+            return a
         else:
             print '[-] %s, dirty'%(a)
-        #sslConnect(domain, directory)
-        #if response.status blah blah
-
-#def prefixRemoval(link):
-#    if link.startswith("https://"): return link[8:]
-#    return link[7:] if link.startswith("http://") else link
+    except requests.ConnectionError:
+        print '[-] %s, timeout error'%(a)
 
 def sslConnect(domain):
     connection = requests.get(domain, timeout=3); return connection
 
+if __name__=="__main__":
+    main()
 #test = sslConnect(sys.argv[1], sys.argv[2])
 #print test
-main()

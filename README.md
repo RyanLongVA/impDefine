1f62a23dc6662898365a80251e68e40f3c33bece
Ryan Long <saferkeymercy@gmail.com>
2017-09-14 14:23:26 -0400

    multiple values fixed, baseline functional, updated README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # impDefine
-Currently developing... I run into a absolute ton of domains that are identical in some fashion during recon (e.g. Blank Page, Repeated Errors, etc.) the point is just hacking up something that searches the entire output for the exact strings you define and spits out the urls/responses thatdon't include what you provided 
+Status: Basic Completition; finished
+
+## Usage
+`python impDefine.py -i <input link list> -s "<string to look for>" *optional* -s "<second string>"`
+Note: if you want to include quotes like for json output, the shell escapes those unless you do \" \" or enclose them in singles -->  '""'
+
+I run into a absolute ton of domains that are identical in some fashion during recon (e.g. Blank Page, Repeated Errors, etc.) the point is just hacking up something that searches the entire output for the exact strings you define and spits out the urls/responses thatdon't include what you provided 
--- impDefine.py
+++ impDefine.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python
-import socket, sys, os, requests, pdb
+import socket, sys, os, requests, pdb, optparse
 import multiprocessing as mp
 def main():
-    clist = filter(None, [clist.replace('\n', '') for clist in open("start.links", "r").readlines()])
+    parser = optparse.OptionParser('python impDefine.py' + ' -i <input link list> -s "<string to look for>" *optional* -s "<second string>"')
+    parser.add_option('-i', dest='iListName', type='string', help='input link list')
+    parser.add_option('-s', dest='fString', type='string', help='provide what to look for', action='append')
+    global options
+    (options, args) = parser.parse_args(sys.argv[1:])
+    if (options.iListName == None) | (options.fString == None):
+        print parser.usage
+        exit(0)
+    clist = filter(None, [clist.replace('\n', '') for clist in open(options.iListName, "r").readlines()])
     cleanList = []
-    pool = mp.Pool(4)
+    pool = mp.Pool(mp.cpu_count())
     cleanList += filter(None, pool.map(startList, clist))
     pool.close()
+def searchRes(respCont):
+    #Here -- Goal
+    for c in options.fString:
+        if c in respCont:
+            return False
+            break
+    return True
+
 
 def startList(a):
     try:
         response = sslConnect(a)
-        if "Path not supported" not in response.content:
+        if searchRes(response.content):
             print '[+] %s, clean'%(a)
             return a
         else:
@@ -22,7 +38,6 @@ def startList(a):
 def sslConnect(domain):
     connection = requests.get(domain, timeout=3); return connection
 
+#Going to have to step through manually to get through to the goal b/c of multiprocessing
 if __name__=="__main__":
     main()
-#test = sslConnect(sys.argv[1], sys.argv[2])
-#print test
